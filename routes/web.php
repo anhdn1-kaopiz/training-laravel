cbb71bf3c4ccc37441d0d0a6fc76d1c3e1d4f9be
anhdn <anhdn1@kaopiz.com>
2025-04-29 10:51:55 +0700

    Implement cookie-based authen

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\TestDbController;
 use Illuminate\Support\Facades\Route;
@@ -12,3 +13,16 @@ Route::get('/practice-db', [TestDbController::class, 'practiceDb']);
 Route::get('/practice-eloquent', [TestDbController::class, 'practiceEloquent']);
 
 Route::resource('posts', PostController::class);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
